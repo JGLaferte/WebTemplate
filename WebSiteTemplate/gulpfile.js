45b3c0d466527e41fc2cdefc946dceb80e29c733
EQUISOFT\JGLaferte <Jean-Gabriel.Laferte@equisoft.com>
2017-11-30 17:16:43 -0500

    init

---
+++ WebSiteTemplate/gulpfile.js
@@ -0,0 +1,79 @@
+﻿//Packages variable 
+var gulp = require('gulp');
+var compileSass = require('gulp-sass');
+var concatenateFiles = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-cssnano');
+var runSequence = require('run-sequence');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+
+
+
+//Structurevariable
+var SassFiles = "./Content/Sass/**/*.scss";
+var sassCompileDestination = "./Content/Css";
+var cssMainFile = sassCompileDestination + "/Site.css";
+
+gulp.task('InitBrowserSync', function () {
+    var files = [
+        'Content/**/*.css'      
+    ];
+
+    browserSync.init(files, {
+
+        proxy: "http://localhost:50135/"
+    });
+});
+
+gulp.task('CompileSass', function () {
+    return gulp.src(SassFiles)
+        .pipe(compileSass())
+        .pipe(gulp.dest(sassCompileDestination));
+});
+
+gulp.task('ConcatCss', function () {
+    return gulp.src
+        ([
+            './Content/Css/NavBar.css',
+            './Content/Css/Jumbotron.css',
+            './Content/Css/Footer.css'
+
+        ])
+        .pipe(concat('Site.css'))
+        .pipe(gulp.dest(sassCompileDestination));
+});
+
+gulp.task('MinifyCss', function () {
+    return gulp.src(cssMainFile)
+        .pipe(rename('Site.min.css'))
+        .pipe(minifyCss())        
+        .pipe(gulp.dest(sassCompileDestination));
+});
+
+gulp.task('Autoprefixing', function() {
+    gulp.src(sassCompileDestination + "/Site.min.css")
+            .pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
+            .pipe(gulp.dest(sassCompileDestination));
+    }
+);
+
+gulp.task('CssCompileFlow', function (callback) {
+    runSequence(['CompileSass'], ['ConcatCss'], ['MinifyCss'], ['Autoprefixing'], ['ReloadBrowser'], callback);
+});
+
+gulp.task('ReloadBrowser', function (callback) {
+    browserSync.reload({
+        stream: true
+    });
+    callback();
+});
+
+
+gulp.task('watch', ['InitBrowserSync'], function () {
+    
+    return gulp.watch(SassFiles, ['CssCompileFlow']);
+});
