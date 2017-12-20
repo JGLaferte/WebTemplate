691cafbee65a2f8f8ca2f0ced6d3605d465050f8
EQUISOFT\JGLaferte <Jean-Gabriel.Laferte@equisoft.com>
2017-12-20 18:14:31 -0500

    init -

---
+++ WebSiteTemplate/Scripts/Site.js
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+function AddSucessNotification(element,message) {
+    $("#NoticerContainer").append('<div class="alert alert-success" role="alert"><strong>success</strong></div>');
+    $('#NoticerContainer').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST'
+        });
+    });
+
+
+}
--- WebSiteTemplate/gulpfile.js
+++ WebSiteTemplate/gulpfile.js
@@ -12,19 +12,9 @@ var purify = require('gulp-purifycss');
 var browserSync = require('browser-sync').create();
 var sourcemaps = require('gulp-sourcemaps');
 var imagemin = require('gulp-imagemin');
+var deleteLines = require('gulp-delete-lines');
 
 
-gulp.task('images', function () {
-    return gulp.src('./Content/Images/**/*.*')
-        .pipe(imagemin([
-                imagemin.gifsicle({ interlaced: true }),
-                imagemin.jpegtran({ progressive: true }),
-                imagemin.optipng({ optimizationLevel: 5 })]),
-                imagemin.svgo({plugins: [{ removeViewBox: true },{ cleanupIDs: true }]})
-                )
-
-        .pipe(gulp.dest('./Content/Distribution'));
-});
 
 
 //Structure Variable
@@ -32,7 +22,56 @@ var SassFiles = "./Content/Sass/**/*.scss";
 var cshtmlFiles = 'Views/**/*.cshtml';
 var sassCompileDestination = "./Content/Css";
 var cssMainFile = sassCompileDestination + "/Site.css";
+var DevScriptFolder = "./Scripts/";
 
+
+gulp.task('ConcatBootstrap', function () {
+    var bootsrapFiles = [
+        "./Scripts/Bootstrap/util.js",
+        "./Scripts/Bootstrap/tooltip.js",
+        "./Scripts/Bootstrap/tab.js",
+        "./Scripts/Bootstrap/scrollspy.js",
+        "./Scripts/Bootstrap/popover.js",
+        "./Scripts/Bootstrap/modal.jss",
+        "./Scripts/Bootstrap/dropdown.js",
+        "./Scripts/Bootstrap/collapse.js",
+        "./Scripts/Bootstrap/carousel.js",
+        "./Scripts/Bootstrap/button.js",
+        "./Scripts/Bootstrap/alert.js"
+    ];
+
+    return gulp.src(bootsrapFiles)
+        .pipe(deleteLines({ 'filters': [/^(import|export)/] }))
+        .pipe(concat("Bootstrap.js"))
+        .pipe(gulp.dest(DevScriptFolder));
+});
+
+gulp.task('ConcatTether', function () {
+    var bootsrapFiles = [
+        "./Scripts/Tether/utils.js",
+        "./Scripts/Tether/tether.js",
+        "./Scripts/Tether/shift.js",
+        "./Scripts/Tether/markAttachment.js",
+        "./Scripts/Tether/constraint.js",
+        "./Scripts/Tether/abutment.js",
+    ];
+
+    return gulp.src(bootsrapFiles)      
+        .pipe(concat("Tether.js"))
+        .pipe(gulp.dest(DevScriptFolder));
+});
+
+gulp.task('images', function () {
+    return gulp.src('./Content/Images/**/*.*')
+        .pipe(imagemin([
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.jpegtran({ progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 })]),
+            imagemin.svgo({ plugins: [{ removeViewBox: true }, { cleanupIDs: true }] })
+        )
+
+        .pipe(gulp.dest('./Content/Distribution'));
+});
 gulp.task('InitBrowserSync', function () {
     var files = [
         'Content/**/*.css',
@@ -75,7 +114,7 @@ gulp.task('Autoprefixing', function () {
 );
 
 gulp.task('CssCompileFlow', function (callback) {
-    runSequence(['CompileSass'], ['PurifyCss'], ['MinifyCss'], ['Autoprefixing'], ['ReloadBrowser'], callback);
+    runSequence(['CompileSass'], ['MinifyCss'], ['Autoprefixing'], ['ReloadBrowser'], callback);
 });
 
 gulp.task('ReloadBrowser', function (callback) {
